b3a1d1a3b369df702ad7f5d4a44f7b04b6626218
ahmedijaz <ahmedijaz0007@gmail.com>
2024-08-07 20:55:41 -0700

    problem 1

--- filehandling.py
+++ filehandling.py
@@ -1,3 +1,22 @@
-import os
+# import os
+# os.remove("sample.txt")
+f = open("sample.txt","r+")
+data = f.read()
+print(data)
 
-os.remove("sample.txt")
+newdata = data.replace("Java","Python")
+
+print(newdata)
+
+
+f.write(newdata)
+
+index =  data.find("learning")
+
+print("learning was found") if index != -1 else print("learning wasn't found")
+
+f.close()
+
+
+
+
